727ad27afdb1d0e89d78cff07714e252ad6d8fda
Daniel Louis <44950643+dtl29@users.noreply.github.com>
2020-07-09 14:45:25 -0400

    Add files via upload

--- Euler.cpp
+++ Euler.cpp
@@ -9,6 +9,9 @@
 #include "Euler8.hpp"
 #include "Euler9.hpp"
 #include "Euler10.hpp"
+#include "Euler11.hpp"
+#include "Euler12.hpp"
+#include "Euler13.hpp"
 
 /*
 Main function to test problems
@@ -26,4 +29,7 @@ int main()
 	//std::cout << std::fixed<< "#8: The largest product is " << Euler8() << std::endl;//answer: 23514624000
 	//std::cout << "#9: The product of a*b*c is " << Euler9() << std::endl;//answer: 31875000
 	//std::cout << std::fixed << "#10: The sum of primes under 2 mill is " << Euler10() << std::endl;//answer: 142913828922
+	std::cout << std::fixed << "#11: The largest product of four numbers in a row is " << Euler11() << std::endl;//answer: 70600674 //gp back to this one
+	//std::cout << "#12: The first triangle number with 500 divisors is " << Euler12() << std::endl;//answer: 76576500
+	std::cout << "#13: The " << Euler13() << std::endl;
 }
